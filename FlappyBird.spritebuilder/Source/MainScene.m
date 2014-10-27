c97b90cc369a6c1227c512689c2d0c27a6e9197e
Dmitriy <Likhachov@Dmitriys-MacBook-Pro.local>
2014-10-27 14:50:49 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -194,7 +194,7 @@ CCParallaxNode *_parallaxBackground;
         if (cloudScreenPosition.x <= (-1 * cloud.contentSize.width)) {
             for (CGPointObject *child in _parallaxBackground.parallaxArray) {
                 if (child.child == cloud) {
-                    child.offset = ccp(child.offset.x + 2*cloud.contentSize.width, child.offset.y);
+//                    child.offset = ccp(child.offset.x + 2*cloud.contentSize.width, child.offset.y);
                 }
             }
         }
